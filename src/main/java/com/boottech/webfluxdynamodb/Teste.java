e47c2b6ded6c81838acb911e3c703dcc18604133
Saltoleto <fernandosaltoleto@hotmail.com>
2023-09-27 14:09:01 -0300

    Create Teste.java

---
+++ src/main/java/com/boottech/webfluxdynamodb/Teste.java
@@ -0,0 +1,29 @@
+import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedAsyncClient
+import software.amazon.awssdk.enhanced.dynamodb.model.QueryConditional
+import java.time.LocalDate
+
+// Crie um objeto DynamoDbEnhancedAsyncClient, substituindo com suas configurações
+val enhancedAsyncClient = DynamoDbEnhancedAsyncClient.builder().build()
+
+// Especifique as datas de início e término que você deseja consultar
+val startDate = LocalDate.parse("2023-09-12")
+val endDate = LocalDate.parse("2023-10-12")
+
+// Especifique o valor do índice que você deseja consultar
+val indexValue = "SeuValorDeIndiceAqui"
+
+// Crie uma condição de consulta para a faixa de datas e o valor do índice
+val queryConditional = QueryConditional
+    .between(
+        QueryConditional.sortKeyBind("data_vencimento"),
+        startDate,
+        endDate
+    )
+    .and(QueryConditional.equalTo(QueryConditional.indexKeyBind("seu_indice"), indexValue))
+
+// Execute a consulta na tabela com o índice GSI "Fernando"
+val table = enhancedAsyncClient.table("NomeDaSuaTabela", YourItem::class.java)
+val gsi = table.index("Fernando") // Substitua "Fernando" pelo nome do seu GSI
+val items = gsi.query(queryConditional).items().toList()
+
+// Agora 'items' contém os itens que atendem à sua condição de consulta usando o GSI e o valor do índice
